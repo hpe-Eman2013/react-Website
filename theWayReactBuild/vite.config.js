e6919576153f4822ee7e5371e6f338077fef3031
Yahman <deman67@protonmail.com>
2026-02-21 13:49:25 -0500

    updated the vite.config.js file to include the proxy settings call to the backend.

--- theWayReactBuild/vite.config.js
+++ theWayReactBuild/vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: "127.0.0.1",
     port: 5173,
     strictPort: true,
+
+    // ✅ Proxy API calls to backend during dev
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   define: {
     "process.env": {},
